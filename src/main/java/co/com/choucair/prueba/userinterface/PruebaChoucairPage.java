6da43f9d2b238142045c60b078c4074d36e4c3f3
Henry Daniel Carvajal Aguilar <henrycarvajalhannie@gmail.com>
2022-01-18 22:21:38 -0500

    Delete carpeta src

--- src/main/java/co/com/choucair/prueba/userinterface/PruebaChoucairPage.java
+++
@@ -1,8 +0,0 @@
-package co.com.choucair.prueba.userinterface;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://utest.com/")
-public class PruebaChoucairPage extends PageObject{
-}
